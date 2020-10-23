1744da7d781ed190bbe0174ebde6d6cea31abf56
karthick3018 <karthickraja3018@gmail.com>
2020-10-24 01:20:48 +0530

    initial work timer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React,{useState} from 'react';
+import useTimerHooks from './hooks/useTimerHooks';
 import './App.css';
 
+const initialState = {
+  seconds:0,
+  minutes:0,
+  hours:0
+}
+
 const App=()=> {
+  const [isTimerStart,setTimerStart] = useState(false);  
+  const [state] = useTimerHooks(initialState,isTimerStart);
+
   return (
      <div>
        Here's wfh mate that can help you
+      <div>
+       <button onClick={()=>setTimerStart(true)}>Start</button>
+       <p>hh:{state?.hours}mm:{state?.minutes}ss:{state?.seconds}</p>
+       <button onClick={()=>alert('yahoo! great work for the day')}>End</button>
+       </div> 
      </div>
   );
 }
---
+++ src/hooks/useTimerHooks.js
@@ -0,0 +1,43 @@
+import {useState,useEffect} from 'react';
+
+const initialState = {
+  seconds:0,
+  minutes:0,
+  hours:0
+}
+
+const useTimerHooks = (existingState=initialState,isStart=true) => {
+
+  const [state, setState] = useState(()=>existingState);
+
+  useEffect(() => {
+    let timer;
+    if(isStart){
+      timer = setTimeout(()=>{
+        if(state?.seconds<59){
+          setState(prevState => {
+           return {...prevState, seconds: prevState.seconds+1};
+         });
+        }
+        if(state?.seconds===59){
+         setState(prevState => {
+           return {...prevState, seconds:0,minutes:prevState.minutes+1};
+         });
+        }
+        if(state?.minutes===59){
+          setState(prevState => {
+           return {...prevState, seconds:0,minutes:0,hours:prevState.hours+1};
+         });
+        }
+      },1000)
+    }
+
+    return () => clearTimeout(timer);
+    
+ },[state,isStart])
+
+ return [state]
+
+}
+
+export default useTimerHooks;
