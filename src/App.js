c6477405d631ac952f327a4564a74db2e646c44c
karthick3018 <karthickraja3018@gmail.com>
2020-12-03 23:49:05 +0530

    clear note added

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ const App=()=> {
         <div className="break-wrap">
           <BreakTimer />
           <WaterBreakTimer />
+          <p className="clear-note">*only this part will reset during page refresh</p>
         </div>
        </div>
      </div>
