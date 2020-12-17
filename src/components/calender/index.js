b230327bdbd83bd21efa0e1cf369db7bc3bf07f8
karthick3018 <karthickraja3018@gmail.com>
2020-12-17 15:56:50 +0530

    calender added

--- src/components/calender/index.js
+++ src/components/calender/index.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React,{useState} from 'react';
+import { Calendar } from 'antd';
 import './calender.css';
 
 const Calender = () => {
-  const date = new Date()
+  const date = new Date();
+  const [isCalenderVisible,setCalenderVisible] = useState(false);
   return (
-    <div className="calender-wrapper">
+    <div className="calender">
+    <div onClick={()=>setCalenderVisible(!isCalenderVisible)} className="calender-wrapper">
       <div className="calender-inner-wrapper">
       <p className="month">{date.toString().split(' ')[1]}</p>
       <p className="date">{date.getDate()<10?`0${date.getDate()}`:date.getDate()}</p>
       <p className="formatted-date">{date.toLocaleDateString()}</p>
       </div>
+    </div>
+      {isCalenderVisible && 
+      <div className="monthly-calender-wrapper">
+      <Calendar className="monthly-calender" fullscreen={false} />
+      </div>
+      }
     </div>
   )
 }
