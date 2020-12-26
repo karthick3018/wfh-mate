62b435a7ad121d886f19662f73aadda82d182a0c
karthick3018 <karthickraja3018@gmail.com>
2020-12-26 20:12:32 +0530

    outside click added

--- src/components/calender/index.js
+++ src/components/calender/index.js
@@ -21,8 +21,8 @@ const Calender = () => {
   const date = new Date();
   const [isCalenderVisible,setCalenderVisible] = useState(false);
   return (
-    <div className="calender">
-    <div ref={calenderRef} onClick={()=>setCalenderVisible(!isCalenderVisible)} className="calender-wrapper">
+    <div className="calender" ref={calenderRef}>
+    <div  onClick={()=>setCalenderVisible(!isCalenderVisible)} className="calender-wrapper">
       <div className="calender-inner-wrapper">
       <p className="month">{date.toString().split(' ')[1]}</p>
       <p className="date">{date.getDate()<10?`0${date.getDate()}`:date.getDate()}</p>
