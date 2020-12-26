6e720e5f48984e5016715e2491e9f12ffdf0d2b6
karthick3018 <karthickraja3018@gmail.com>
2020-12-26 20:09:54 +0530

    outside click added

--- src/components/calender/index.js
+++ src/components/calender/index.js
@@ -1,13 +1,28 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect,useRef} from 'react';
 import { Calendar } from 'antd';
 import './calender.css';
 
 const Calender = () => {
+  const calenderRef = useRef(null);
+  
+  useEffect(() => {
+    document.addEventListener("click", handleOutsideClick, false);
+    return () => {
+      document.removeEventListener("click", handleOutsideClick, false);
+    };
+  }, []);
+
+  const handleOutsideClick = (e) => {
+    if (calenderRef.current && !calenderRef.current.contains(e.target)) {
+      setCalenderVisible(false);
+    }
+  }
+
   const date = new Date();
   const [isCalenderVisible,setCalenderVisible] = useState(false);
   return (
     <div className="calender">
-    <div onClick={()=>setCalenderVisible(!isCalenderVisible)} className="calender-wrapper">
+    <div ref={calenderRef} onClick={()=>setCalenderVisible(!isCalenderVisible)} className="calender-wrapper">
       <div className="calender-inner-wrapper">
       <p className="month">{date.toString().split(' ')[1]}</p>
       <p className="date">{date.getDate()<10?`0${date.getDate()}`:date.getDate()}</p>
