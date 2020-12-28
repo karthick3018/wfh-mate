bcb9b240a479374d3495a0ce3e9f4b8a15cbcc1a
karthick3018 <karthickraja3018@gmail.com>
2020-12-29 00:00:01 +0530

    Updates

---
+++ precache-manifest.a7e49481258aa46b63a4de484b84d51b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a61317f33fd4eae22e81aac9f6ba4dd",
+    "url": "/wfh-mate/index.html"
+  },
+  {
+    "revision": "1d74d5623994b13d8732",
+    "url": "/wfh-mate/static/css/2.00a338ba.chunk.css"
+  },
+  {
+    "revision": "8314edfd34c8121333d3",
+    "url": "/wfh-mate/static/css/main.2fab71a8.chunk.css"
+  },
+  {
+    "revision": "1d74d5623994b13d8732",
+    "url": "/wfh-mate/static/js/2.0e7df2a0.chunk.js"
+  },
+  {
+    "revision": "ce53c4b16522e4a5817476f862996bac",
+    "url": "/wfh-mate/static/js/2.0e7df2a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8314edfd34c8121333d3",
+    "url": "/wfh-mate/static/js/main.2feee073.chunk.js"
+  },
+  {
+    "revision": "b91e329cc02a7ae47f4e",
+    "url": "/wfh-mate/static/js/runtime-main.492eb709.js"
+  },
+  {
+    "revision": "985ddc7df54230a3dc71f0a9725c3300",
+    "url": "/wfh-mate/static/media/mate.985ddc7d.png"
+  },
+  {
+    "revision": "35493834046bea95cbafe20972460ac6",
+    "url": "/wfh-mate/static/media/minus.35493834.svg"
+  },
+  {
+    "revision": "ef083f734edeb464da79750a9e852bde",
+    "url": "/wfh-mate/static/media/play.ef083f73.svg"
+  },
+  {
+    "revision": "4874204c6bc16ed503e4abb2eea63891",
+    "url": "/wfh-mate/static/media/plus.4874204c.svg"
+  },
+  {
+    "revision": "e412b71f2326e4683e5de9387e4ce3ae",
+    "url": "/wfh-mate/static/media/stop.e412b71f.svg"
+  },
+  {
+    "revision": "ffc2106245ca3d35e85e8b94e42219ce",
+    "url": "/wfh-mate/static/media/tired.ffc21062.png"
+  }
+]);
